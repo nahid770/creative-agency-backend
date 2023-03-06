d9b178a5ff83bd0483b01b0c681a5d4dbcf95db8
nahid770 <nurnahid10@gmail.com>
2023-03-06 19:06:46 +0600

    changes updated

--- index.js
+++ index.js
@@ -55,21 +55,22 @@ async function run(){
     const cursor  = servicesCollection.find(query);
     const result = await cursor.toArray();
     res.send(result)
-
+  },
     // ----- READ ----- Get a Single service for serviceDetails page
     app.get('/services/:id', async(req, res)=>{
       const id = req.params.id;
       const query = { _id : new ObjectId(id) }
       const service = await servicesCollection.findOne(query);
       res.send(service);
-
+    }),
+    
       // ----- READ ----- Get Single service for checkout page
       app.get('/checkout/:id', async(req, res)=>{
         const id = req.params.id;
         const query = { _id : new ObjectId(id)}
         const result = await servicesCollection.findOne(query);
         res.send(result);
-      })
+      }),
 
       // ----------- Order API ----------
       // ----- READ ----- Get all orders for a user using Get & query
@@ -88,14 +89,14 @@ async function run(){
         const cursor = ordersCollection.find(query);
         const orders = await cursor.toArray();
         res.send(orders);
-      }) 
+      }),
 
       // --- CREATE ---  using post 
       app.post('/orders', async(req, res)=>{
         const order = req.body;
         const result = await ordersCollection.insertOne(order);
         res.send(result);
-      })
+      }),
 
       // --- UPDATE ---- 
       app.patch('/orders/:id', async(req, res)=>{
@@ -109,7 +110,7 @@ async function run(){
         }
         const result = await ordersCollection.updateOne(query, updatedDoc);
         res.send(result);
-      })
+      }),
 
       // --- DELETE --- Order
       app.delete('/orders/:id', async(req, res)=>{
@@ -117,10 +118,10 @@ async function run(){
         const query = {_id : new ObjectId(id)};
         const result = await ordersCollection.deleteOne(query);
         res.send(result);
-      })
+      }),
 
-    })
-})} 
+   
+)} 
     finally {
       // await client.close();
     }
